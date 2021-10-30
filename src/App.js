c57cdf65b62e2408e6c0a46a463695a063739bd3
William McGuire <wmcguire18@gmail.com>
2021-10-30 15:03:00 -0400

    Add search bar

--- src/App.js
+++ src/App.js
@@ -21,13 +21,12 @@ class App extends Component {
       <main className='App'>
       <nav className="nav-bar">
         <p className="nav-bar__app-title">The Drip List</p>
+        <Form />
         <div className="nav-bar__language-logo">
         <img className='nav-bar__english' src={English} alt="English language"/>
         </div>
       </nav>
-      <body>
       <RecipeContainer recipes={this.state.recipes}/>
-      </body>
       </main>
     )
   }
