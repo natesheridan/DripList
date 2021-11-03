2e42bba6cb95a7bcb09ba0e4a261c2765e640ec0
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-11-03 13:56:17 -0600

    Add styling of featured recipes to welcome/home page

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import RecipeCard from '../RecipeCard/RecipeCard.js';
 import './Welcome.css';
-import { getRandomRecipe } from './../../APICalls'
+import { getRandomRecipe, getLatestRecipes } from './../../APICalls'
 import { useAuth0 } from '@auth0/auth0-react';
 
 
@@ -9,50 +9,54 @@ const Welcome = () => {
   const  userData = {}
   
   const [randomDrink, setRandomDrink] = React.useState([])
+  const [drinks, setDrinks] = React.useState([])
 
   React.useEffect(() => {
     getRandomRecipe()
     .then(data => {
       setRandomDrink(data.drinks[0])
-      console.log(randomDrink)
+    })
+    getLatestRecipes()
+    .then(data => {
+      setDrinks(data.drinks[0])
     })
   }, [])
 
   const { loginWithRedirect } = useAuth0()
 
   return (
-    <section className="welcome-section">
-      <div className="wbox log-in-section">
-        <p> Welcome to </p>
-        <p className="wbox-logo">
-          <span className="material-icons indigo">water_drop</span>DripList
-        </p>
-        <p className="s"> Where you curate the drink list! </p>
-        {true
-          ?<>
-            <p>Please sign in with the buttons below:</p>
-            <div>
-              <button onClick={() => loginWithRedirect()}>Login</button>
-            </div>
-          </>
-          :<>
-            <p>Hi [Firstname Lastname]!</p> {/* userdata.Name? */}
-            <p>Visit your saved <a href="">DripList</a></p> {/* userdata.Name?  also replace a tags with NavLink after profile nav is setup*/} 
-            <p>Find a new favorite below or search by ingredient above!</p> {/* userdata.Name? */}
-          </>}
-      </div>
-      <div className="wbox featured-drink">
-        <h3>Have you tried</h3>
-        <div className="featured-recipe">
-            {/* <RecipeCard/> */}
-            <h3>{randomDrink?.strDrink}</h3>
-            <img className="wbox-featured-img" alt={randomDrink?.strDrink} src={randomDrink?.strDrinkThumb}></img>
-            <div className="wbox-featured-buttons">
-              <button>See the Recipe</button>
-            </div>
+      <section className="welcome-section">
+        <div className="wbox log-in-section">
+          <p> Welcome to </p>
+          <p className="wbox-logo">
+            <span className="material-icons indigo">water_drop</span>DripList
+          </p>
+          <p className="s"> Where you curate the drink list! </p>
+          {true
+            ?<>
+              <p>Please sign in with the buttons below:</p>
+              <div>
+                <button onClick={() => loginWithRedirect()}>Login</button>
+              </div>
+            </>
+            :<>
+              <p>Hi [Firstname Lastname]!</p> {/* userdata.Name? */}
+              <p>Visit your saved <a href="">DripList</a></p> {/* userdata.Name?  also replace a tags with NavLink after profile nav is setup*/} 
+              <p>Find a new favorite below or search by ingredient above!</p> {/* userdata.Name? */}
+            </>}
         </div>
-      </div>
-    </section>
+        <div className="wbox featured-drink">
+          <h3>Have you tried</h3>
+          <div className="featured-recipe">
+              {/* <RecipeCard/> */}
+              <h3>{randomDrink?.strDrink}</h3>
+              <img className="wbox-featured-img" alt={randomDrink?.strDrink} src={randomDrink?.strDrinkThumb}></img>
+              <div className="wbox-featured-buttons">
+                <button>See the Recipe</button>
+              </div>
+          </div>
+        </div>
+      </section>
   )
 }
 
