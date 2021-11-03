3880f4daac8fa2422f96a0c66dd7e7defddf68f1
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-11-03 11:22:13 -0600

    Sanity check

--- src/APICalls.js
+++ src/APICalls.js
@@ -7,5 +7,10 @@ export const getSingleRecipe = (id) => {
   return fetch(`https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=${id}`)
 }
 
+export const getRandomRecipe = () => {
+  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+  .then(response => response.json())
+}
+
 //https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=
 // ^^ big api with 627 drinks. I used the 'latest' list so it wasn't so huge.
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,15 +1,54 @@
 import React from 'react';
 import RecipeCard from '../RecipeCard/RecipeCard.js';
 import './Welcome.css';
+import { getRandomRecipe } from './../../APICalls'
 
 const Welcome = () => {
+  const  userData = {}
+  
+  const [randomDrink, setRandomDrink] = React.useState([])
+
+  React.useEffect(() => {
+    getRandomRecipe()
+    .then(data => {
+      setRandomDrink(data.drinks[0])
+      console.log(randomDrink)
+    })
+  }, [])
+
   return (
     <section className="welcome-section">
-      <div className="log-in-section">
-        <p> Log In Section - Coming Soon </p>
+      <div className="wbox log-in-section">
+        <p> Welcome to </p>
+        <p className="wbox-logo">
+          <span className="material-icons indigo">water_drop</span>DripList
+        </p>
+        <p className="s"> Where you curate the drink list! </p>
+        {false
+          ?<>
+            <p>Please sign in with the buttons below:</p>
+            <div>
+              <button>google</button>
+              <button>facebook</button>
+              <button>github</button>
+            </div>
+          </>
+          :<>
+            <p>Hi [Firstname Lastname]!</p> {/* userdata.Name? */}
+            <p>Visit your saved <a href="">DripList</a></p> {/* userdata.Name?  also replace a tags with NavLink after profile nav is setup*/} 
+            <p>Find a new favorite below or search by ingredient above!</p> {/* userdata.Name? */}
+          </>}
       </div>
-      <div className="featured-drink">
-        <p> Featured Drink - Coming Soon </p>
+      <div className="wbox featured-drink">
+        <h3>Have you tried</h3>
+        <div className="featured-recipe">
+            {/* <RecipeCard/> */}
+            <h3>{randomDrink?.strDrink}</h3>
+            <img className="wbox-featured-img" alt={randomDrink?.strDrink} src={randomDrink?.strDrinkThumb}></img>
+            <div className="wbox-featured-buttons">
+              <button>See the Recipe</button>
+            </div>
+        </div>
       </div>
     </section>
   )
