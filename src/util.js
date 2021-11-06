0c247ace0c8c21971e1336494a1f7549bd63970f
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-11-06 16:52:18 -0600

    Store data as JSON Objects inside of LS refactor

--- src/util.js
+++ src/util.js
@@ -29,37 +29,61 @@
 
 
 
-export const toggleInStorage = (userSubID, drinkID) => { //userSUB id google-oauth2|1105692342342342348 
-  const userLS = localStorage[userSubID]
-  // userLS is localstorage.google-oauth2|1105692342342342348
-  // which is a string of numbers
-  // split by 6 below because each drink recipe id is 6 numbers
+// export const toggleInStorage12 = (userSubID, drinkObj) => { //userSUB id google-oauth2|1105692342342342348 
+//   const userLS = localStorage[userSubID]
+//   // userLS is localstorage.google-oauth2|1105692342342342348
+//   //stored in UserLS is an JSON object
+//   let ls = {};
 
 
-  if (!userLS){
-  //if the localStorage.userID is undefined set just the first saved drink as the in an array
-    localStorage.setItem(userSubID, drinkID)
-    return [drinkID]
-  };
 
-  let userLSArr = userLS.match(/.{1,6}/g)
-  //split the string of numbers every 6 numbers(characters)
-  const indexIfFavorite = userLSArr.findIndex(id => id===drinkID)
+//   if (!userLS){
+//   //if the localStorage.userID is undefined set just the first saved drink as the in an array
 
-  //these blocks are the same except for line 51&57
-  if (indexIfFavorite>-1){ //if LS has ID saved already
-    userLSArr.splice(indexIfFavorite, 1); //then we want to remove where it was found with (indexIfFavorite)
-    const userLSArrStr = userLSArr.join(''); 
-    localStorage.setItem(userSubID, userLSArrStr);
-    return userLSArr;   
+//     localStorage.setItem(userSubID, drinkID)
+//     return [drinkID]
+//   };
+
+//   let userLSArr = userLS.match(/.{1,6}/g)
+//   //split the string of numbers every 6 numbers(characters)
+//   const indexIfFavorite = userLSArr.findIndex(id => id===drinkID)
+
+//   //these blocks are the same except for line 51&57
+//   if (indexIfFavorite>-1){ //if LS has ID saved already
+//     userLSArr.splice(indexIfFavorite, 1); //then we want to remove where it was found with (indexIfFavorite)
+//     const userLSArrStr = userLSArr.join(''); 
+//     localStorage.setItem(userSubID, userLSArrStr);
+//     return userLSArr;   
     
-  } else {
-    userLSArr.push(drinkID); //while here we are just adding the drink id to the array
-    const userLSArrStr = userLSArr.join('')//then joining the array back together with no spaces in between
-    localStorage.setItem(userSubID, userLSArrStr)//setting that (userLSArrStr) string of numbers at the subID localStorage key
-    return userLSArr //returning the all drink ids that are in localStorage from user key after changing data
+//   } else {
+//     userLSArr.push(drinkID); //while here we are just adding the drink id to the array
+//     const userLSArrStr = userLSArr.join('')//then joining the array back together with no spaces in between
+//     localStorage.setItem(userSubID, userLSArrStr)//setting that (userLSArrStr) string of numbers at the subID localStorage key
+//     return userLSArr //returning the all drink ids that are in localStorage from user key after changing data
     
 
+//   }
+// }
+
+
+
+
+export const toggleInStorage  = (userSubID, recipeObj) => {
+  const initialLocalStorage = JSON.parse(localStorage.getItem(userSubID));
+  if(!initialLocalStorage){
+    let defaultArr = [recipeObj]
+    localStorage.setItem(userSubID, JSON.stringify(defaultArr))
+    return defaultArr
+  }
+  const indexIfFoundInStorage = initialLocalStorage.findIndex((recipe) => recipe.idDrink  === recipeObj.idDrink)
+  if(indexIfFoundInStorage>-1){
+    initialLocalStorage.splice(indexIfFoundInStorage, 1)
+    localStorage.setItem(userSubID, JSON.stringify(initialLocalStorage))
+    return initialLocalStorage
+  }else{
+    initialLocalStorage.push(recipeObj)
+    localStorage.setItem(userSubID, JSON.stringify(initialLocalStorage))
+    return initialLocalStorage
   }
 }
 
