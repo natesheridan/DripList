eb0b972e5bc39384463ee1a61ab6549c34ea017e
Anna Johnson <ae.johnson2931@gmail.com>
2021-11-06 15:56:55 -0700

    Add styling and fix recipe detail bug to vodka page

--- src/components/Vodka/Vodka.js
+++ src/components/Vodka/Vodka.js
@@ -17,31 +17,26 @@ const Vodka = () => {
   const vodkaRecipeCard = vodkaContainer.map(recipe => {
       return (
       <div>
-        <Link to={`/drinks/vodka`}>
+        <Link to={`/recipe/${recipe.idDrink}`}>
           <RecipeCard
-            name={recipe.strDrink}
-            ingredients1={recipe.strIngredient1}
-            ingredients2={recipe.strIngredient2}
-            ingredients3={recipe.strIngredient3}
-            ingredients4={recipe.strIngredient4}
-            ingredients5={recipe.strIngredient5}
-            ingredients6={recipe.strIngredient6}
-            ingredients7={recipe.strIngredient7}
-            ingredients8={recipe.strIngredient8}
-            ingredients9={recipe.strIngredient9}
-            ingredients10={recipe.strIngredient10}
-            ingredients11={recipe.strIngredient11}
-            ingredients12={recipe.strIngredient12}
-            ingredients13={recipe.strIngredient13}
-            ingredients14={recipe.strIngredient14}
-            ingredients15={recipe.strIngredient15}
-            instructions={recipe.strInstructions}
+          name={recipe.strDrink}
+          image={recipe.strDrinkThumb}
+          tags={recipe.strTags}
             />
         </Link>
       </div>
     )
   })
-    return <div className="vodka-recipe-container"> {vodkaRecipeCard} </div>
+  return (
+    <>
+    <div className="drinks-title">
+    <h3 className="featured-drinks">Vodka Drinks</h3>
+    </div>
+    <div className="featured-recipe-container">
+     {vodkaRecipeCard}
+     </div>
+     </>
+  )
 }
 
 
