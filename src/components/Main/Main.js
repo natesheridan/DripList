8b002dec099b0f5ee0ebecfccf22e070bb89a248
Anna Johnson <ae.johnson2931@gmail.com>
2021-11-06 15:56:14 -0700

    Last commit should have said featured file not main. This commit cleans up main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -24,7 +24,7 @@ const Main = () => {
   const [filteredDrinks, setFilteredDrinks] = useState([])
   const [searchValue, setSearchValue] = useState('')
 
-  React.useEffect(() => {
+  useEffect(() => {
     getLatestRecipes()
     .then(data => {
       setAllDrinks(data.drinks)
@@ -44,15 +44,15 @@ const Main = () => {
       if (searchValue.length > 0) {
         return (
           <>
-          <Form handleChange={handleChange}/>
-          <FeaturedRecipeContainer recipes={filteredDrinks}/>
+            <Form handleChange={handleChange}/>
+            <FeaturedRecipeContainer recipes={filteredDrinks}/>
           </>
         )
       } else {
         return (
           <>
-          <Form handleChange={handleChange}/>
-          <FeaturedRecipeContainer recipes={allDrinks}/>
+            <Form handleChange={handleChange}/>
+            <FeaturedRecipeContainer recipes={allDrinks}/>
           </>
         )
       }
@@ -64,52 +64,37 @@ const Main = () => {
           <Route
             exact path="/"
             render= {() => {
-          return (
-          <>
-            <Welcome/>
-            {isAuthenticated && <FeaturedRecipeContainer recipes={allDrinks}/>}
-          </>
-          )}}
+              return (
+                <>
+                  <Welcome/>
+                  {isAuthenticated && <FeaturedRecipeContainer recipes={allDrinks}/>}
+                </>
+              )
+            }}
           />
 
 
-          <Route exact path="/drinks" render={displayRecipes}/>
-
-
+          <Route exact path="/drinks"
+          render={displayRecipes}
+          />
           <Route
-          exact path="/drinks/vodka"
-          render= {() => {
-            return (<Vodka />)
-          }}
+            exact path="/drinks/vodka"
+            render= {() => <Vodka />}
           />
-
-
           <Route
           exact path="/drinks/tequila"
-          render= {() => {
-            return (<Tequila />)
-          }}
+          render= {() => <Tequila />}
           />
-
-
           <Route
           exact path="/drinks/rum"
-          render= {() => {
-            return (<Rum />)
-          }}
+          render= {() => <Rum />}
           />
-
-
           <Route
           exact path="/driplist"
-          render= {() => {
-            return (<Favorites />)
-          }}
+          render= {() => <Favorites />}
           />
-
-
           <Route
-          exact path="/drinks/:id"
+          exact path="/recipe/:id"
           render={({match}) => {
             return <RecipeDetails id={match.params.id} />
           }}
@@ -121,33 +106,3 @@ const Main = () => {
 }
 
 export default Main
-
-
-//             + DrinkCardContainer.js //////////
-//                 Array to pass in as data is latest drinks from /latest API endpoint
-//                 DrinkCardContainer is ideally a component that can scale to any size
-//                 it should be able to wrap items based on viewport width and also
-//     -/drinks
-//         /Drinks can be used to display a full page of drinks
-//         this is where users are able to search all drinks and maybe filter drink recipes?
-//             Two components to render when /drinks router endpoint is it
-//                 + Search.js ////////////////////
-//                     Search ideally can search at the bare minimum but then also can be used to
-//                     filter data as well
-//                 + DrinkCardContainer.js ////////
-//                     Array to pass in as data is all drinks from /search ?q="" API endpoint
-//                     Make sure it stops after 25-50 drinks - or somehow renders in as you scroll?
-//                     DrinkCardContainer is ideally a component that can scale to any size
-//                     it should be able to wrap items based on viewport width and also
-//     -/saved
-//         /Saved is shown as a button in Navbar only when User.IsAuth=true
-//              if !user.isAuth display login button?
-//         otherwise this is the same the /drinks route but takes in data from ?user.Favorites?
-//         ^^^^^^^^^^^^^^^^^
-//         THIS IS WHERE WE WILL NEED EXPRESS SERVER TO STORE OUR USERS FAVORITES
-
-//     -/profile
-//         /Profile is also only shown as a button when user is auth'd.
-//         This route rendered can just be a place to view/edit profile settings
-//             One component:
-//             Profile.js?
