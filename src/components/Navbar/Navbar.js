c0a3a50b8e5d12c7127f3ebe7a0a65c9ad25807a
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-11-06 11:45:42 -0600

    Add extra All drinks navbar button and styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 // needs to have react-router installed via npm install before working
 import './Navbar.css';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h2 className="nav-logo"><span className="material-icons">water_drop</span>DripList</h2>
+            <h2 className="nav-logo"><Link to="/"><span className="material-icons">water_drop</span>DripList</Link></h2>
+            <NavLink to="/drinks">
+              <button>All Drinks</button>
+            </NavLink>
             <NavLink to="/drinks/vodka">
               <button>Vodka</button>
             </NavLink>
