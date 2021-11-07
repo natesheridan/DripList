6d957d4edfe4d54c7386eaea2c89e4a8ea31e29e
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-11-07 01:39:50 -0700

    Edit buttons to be : View DL // Clear DL // Logout

--- src/components/UserDropdownMenu/UserDropdownMenu.js
+++ src/components/UserDropdownMenu/UserDropdownMenu.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './UserDropdownMenu.css'
 import LogoutButton from '../LogoutButton/LogoutButton'
+import { useAuth0 } from '@auth0/auth0-react';
+import { clearUserStorage } from './../../util'
 
 
 const UserDropdownMenu = () => {
+    const {user} = useAuth0()
     return (
         <div className="user-dropdown-menu">
-            <button className="user-btn"><Link to="/driplist" className="test">View Profile</Link></button>
-            <button className="user-btn">Edit Profile</button>
+            <Link to="/driplist" className="user-btn test">View DripList</Link>
+            <Link onClick={() => {clearUserStorage(user.sub)}} className="user-btn test">Clear DripList</Link>
             <LogoutButton/>
         </div>
     )
