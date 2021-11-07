2165675a500b23d82a00973a47573a3e4eac53e0
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-11-07 01:47:28 -0700

    Add preventDefault to prevent buttonclick breaking clearing storage"

--- src/components/UserDropdownMenu/UserDropdownMenu.js
+++ src/components/UserDropdownMenu/UserDropdownMenu.js
@@ -11,7 +11,7 @@ const UserDropdownMenu = () => {
     return (
         <div className="user-dropdown-menu">
             <Link to="/driplist" className="user-btn test">View DripList</Link>
-            <Link onClick={() => {clearUserStorage(user.sub)}} className="user-btn test">Clear DripList</Link>
+            <Link onClick={(e) => {e.preventDefault();clearUserStorage(user.sub)}} className="user-btn test">Clear DripList</Link>
             <LogoutButton/>
         </div>
     )
