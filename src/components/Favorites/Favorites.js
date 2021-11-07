6e1377b6ba9f7535521b943a52d3f689068883a7
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-11-07 01:23:08 -0700

    Working with util.js to display LS favorites

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -4,10 +4,12 @@ import RecipeCard from '../RecipeCard/RecipeCard';
 import { Link } from 'react-router-dom';
 import './Favorites.css';
 import { useAuth0 } from '@auth0/auth0-react';
+import { getUserStorage } from '../../util';
 
 const Favorites = () => {
   const {user} = useAuth0()
   const [favorites, setFavorites] = useState(JSON.parse(localStorage.getItem(user.sub)))
+  console.log(favorites)
   const recipeCards = favorites?.map(recipe => {
     return (
       <Link to={`/recipe/${recipe.idDrink}`}>
@@ -25,7 +27,7 @@ const Favorites = () => {
           <div className="drinks-title">
           <h3 className="featured-drinks">Your DripList</h3>
           </div>
-      {(recipeCards.length>0)
+      {(getUserStorage(user.sub).length>0)
         ?recipeCards
         :<p className="drinks-title white">You seem thirsty! Go find some drinks!</p>
       }
