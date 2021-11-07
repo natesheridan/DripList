be0bcc2dceb577ef5cef060fc4d9eb497ab31637
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-11-06 22:17:48 -0600

    Add loading screen

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import LoadingScreen from './components/LoadingScreen/LoadingScreen'
 import { useAuth0 } from '@auth0/auth0-react';
 
 const App = () => {
 
   return(
     <>
-    {!useAuth0().isLoading &&
-      <>
+    {!useAuth0().isLoading 
+    ?<>
         <Header/>
         <Main/>
-      </>
+    </>
+    :<>
+        <LoadingScreen/>
+    </>
     }
     </>
   )
